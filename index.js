e2ebaa34f1c8719a4c4edad53765eac0fc62705c
newbienewbie <webbot@sina.com>
2016-09-11 22:45:09 +0800

    新增Carousel组件

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Pagination=require('./dist/pagination/index.js');
+const Carousel=require('./dist/carousel/index.js');
 
 
-
-module.exports= {Pagination};
+module.exports= {Pagination,Carousel};
