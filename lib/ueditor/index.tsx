f6d22e26a9777a02b66fb00c5eead70d0c2744aa
newbienewbie <webbot@sina.com>
2016-11-20 20:47:36 +0800

    添加UEditor组件的初始化内容属性

--- lib/ueditor/index.tsx
+++ lib/ueditor/index.tsx
@@ -2,27 +2,34 @@ import * as React from  'react';
 
 declare var UE: any;
 
+export interface UEditorProps{
+    id: string;
+    name: string;
+    height: number;   
+    width: number;    
+    initialContent:string;
+    uconfigSrc: string;
+    ueditorSrc: string;
+}
+
+
+
 /**
- * 百度UEditor的封装
- * <Ueditor id="ueditorContainer" name="content" width={800} height={500} 
- *     uconfigSrc='/static/ueditor/ueditor.config.js'
- *     ueditorSrc='/static/ueditor/ueditor.all.min.js'
- * />
+ * 百度UEditor的React封装
  */
-export const UEditor = React.createClass({
+export class UEditor extends React.Component<UEditorProps,any>{
 
-    getDefaultProps:function () {
-        return{
-            id:'ueditorcontainer',
-            name:'uecontent',
-            height:600,    // 注意这里只能是数字，不可有单位
-            width:600,     // 注意这里只能是数字，不可有单位
-            uconfigSrc:"/static/ueditor/ueditor.config.js",
-            ueditorSrc:"/static/ueditor/ueditor.all.min.js",
-        };
-    },
+    static defaultProps:UEditorProps={
+        id:'ueditorcontainer',
+        name:'uecontent',
+        height:600,    // 注意这里只能是数字，不可有单位
+        width:600,     // 注意这里只能是数字，不可有单位
+        initialContent:'',
+        uconfigSrc:"/static/ueditor/ueditor.config.js",
+        ueditorSrc:"/static/ueditor/ueditor.all.min.js",
+    };
 
-    componentWillMount:function(){
+    componentWillMount(){
         if(typeof UE !='undefined'){
             // 如果UE已经是全局变量了，则说明已经加载了UEditor相应的<script>
             return;
@@ -40,9 +47,9 @@ export const UEditor = React.createClass({
                 document.body.appendChild(scriptEditor);
             }
         }
-    },
+    }
 
-    componentDidMount() {
+    componentDidMount(){
         function waitUntil(props){
             try{
                 let ue = UE.getEditor(props.id, {
@@ -55,25 +62,25 @@ export const UEditor = React.createClass({
             }
         }
         waitUntil(this.props);
-    },
+    }
 
     componentWillUnmount(){
         UE.delEditor(this.props.id);
-        // 不再卸载 :
+        // 不再卸载<script> :
         //     <script type="text/javascript" src="/static/ueditor/ueditor.config.js"></script>
         //     <script type="text/javascript" src="/static/ueditor/ueditor.all.min.js"></script>
-    },
+    }
     
-    render: function () {
-        return ( <script id={this.props.id} name={this.props.name} type="text/plain"> </script> );
+    render(){
+        let initialContent=Object.assign(this.props.initialContent);
+        return (<script 
+            id={this.props.id} name={this.props.name} 
+            dangerouslySetInnerHTML={{__html:initialContent}}
+            type="text/plain"></script>);
     }
 
-});
+}
 
-UEditor.propTypes={
-    height:React.PropTypes.number,
-    width:React.PropTypes.number,
-};
 
 
 
