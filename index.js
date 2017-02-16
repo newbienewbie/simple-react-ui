bd22b09cc00e83df6de7de700f68477c289dd405
newbienewbie <webbot@sina.com>
2017-02-16 15:07:02 +0800

    bugfix：修复统一入口导入错误

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Pagination=require('./dist/pagination/index.js');
 const Carousel=require('./dist/carousel/index.js');
 const UEditor=require('./dist/ueditor/index.js');
-const BaiduMap=require('./dist/baidu-map.js');
+const BaiduMap=require('./dist/baidu-map/baidu-map.js');
 
 
 module.exports= {Pagination,Carousel,UEditor,BaiduMap};
