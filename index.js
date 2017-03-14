44cd93cc6c8f6905d8ada22fc29c63de7363885f
newbienewbie <webbot@sina.com>
2017-03-14 13:12:48 +0800

    把 index.js 移至lib/index.ts

--- index.js
+++
@@ -1,7 +0,0 @@
-const Pagination=require('./dist/pagination');
-const Carousel=require('./dist/carousel');
-const UEditor=require('./dist/ueditor');
-const BaiduMap=require('./dist/baidumap');
-const Icon=require('./dist/icon');
-
-module.exports= {Pagination,Carousel,UEditor,BaiduMap,Icon};
---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import Pagination from './pagination';
+import Carousel from'./carousel';
+import UEditor from './ueditor';
+import BaiduMap from './baidumap';
+import * as Icon from './icon';
+
+
+export {
+    Pagination,
+    Carousel,
+    UEditor,
+    BaiduMap,
+    Icon,
+} 
